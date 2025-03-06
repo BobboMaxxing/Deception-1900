cd3546a9c48a6e901c65761b18e591795681babc
Viktor Myrhill <viktor.myrhill@learnet.se>
2025-03-06 10:15:21 +0100

    fix main scene

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("EventSystem")]
+    public GameObject mainMenuFirstButton;
+    public GameObject settingsFirstButton;
+    public GameObject QuitGameFirstButton;
+
+    [Header("Panels+Ani")]
     public GameObject settingsPanel;
     public Animator settingsAnimator;
+    public GameObject CloseGamePanel;
+
+   
 
     public void OpenSettings()
     {
@@ -12,22 +24,51 @@ public class MainMenuController : MonoBehaviour
         {
             settingsPanel.SetActive(true);
             settingsAnimator.SetTrigger("Open");
+
+            if (settingsFirstButton != null)
+            {
+                EventSystem.current.SetSelectedGameObject(settingsFirstButton);
+            }
         }
     }
+
     public void CloseSettings()
     {
         if (settingsPanel != null && settingsAnimator != null)
         {
             settingsAnimator.SetTrigger("Close");
+
+            if (mainMenuFirstButton != null)
+            {
+                EventSystem.current.SetSelectedGameObject(mainMenuFirstButton);
+            }
         }
     }
+
+    public void CloseGame()
+    {
+        if (CloseGamePanel != null)
+        {
+            CloseGamePanel.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(QuitGameFirstButton);
+        }
+   
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    public void QuitGame()
+    public void QuitGameYes()
     {
         Application.Quit();
+    }
+
+    public void QuitGameNo()
+    {
+        CloseGamePanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(mainMenuFirstButton);
+
     }
 }
